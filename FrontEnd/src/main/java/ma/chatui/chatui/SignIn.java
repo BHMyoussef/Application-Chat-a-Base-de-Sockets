850e0ab996fc77ea6058308390596c051de2c19b
Aymane Daif <aymaned345@gmail.com>
2021-12-26 19:05:32 +0100

    reverting to 48d195e260b75ea3a4d488b60f09631f66475fb5

--- FrontEnd/src/main/java/ma/chatui/chatui/SignIn.java
+++ FrontEnd/src/main/java/ma/chatui/chatui/SignIn.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 @Component
-public class SignIn {
+public class SignIn  extends Registration{
     @FXML
     private TextField email;
     @FXML
@@ -21,21 +21,19 @@ public class SignIn {
     @FXML
     private Label passwordErrorMessage;
 
-    private boolean isUserValid = false;
 
     public void SignInHandler(ActionEvent event) throws IOException {
-        updateBoxes();
-        if(isUserValid) {
-            GoSignUp(event);
-        }
+        registrationHandler(event,"/SignUpUi.fxml");
     }
-    public void GoSignUp(ActionEvent event) throws IOException {
-        ScenesController scenesController = new ScenesController();
-        scenesController.switchScene(event, "/SignUpUi.fxml");
+
+    public void goSignUp(ActionEvent event){
+        goTo(event, "/SignUpUi.fxml");
     }
+
+    @Override
     public void updateBoxes(){
         if(!isEmptyBox(this.email)){
-            updateEmailBox();
+            updateEmailBox(this.email, this.emailErrorMessage);
         }else {
             updateEmptyBox(this.email);
         }
@@ -45,48 +43,13 @@ public class SignIn {
             updateEmptyBox(this.password);
     }
 
-    public void updateEmptyBox(TextField field) {
-        if(isEmptyBox(field)){
-            changeBoxStyle(field, SignUp.errorBorder,true);
-            getAttribute(field.getId()).setText(field.getId()+" cannot be empty");
-
-            isUserValid=false;
-        } else{
-            changeBoxStyle(field, SignUp.validBorder,false);
-        }
-    }
-
-    public void updateEmailBox(){
-        if(isEmailValid(this.email)){
-            changeBoxStyle(this.email, SignUp.validBorder,false);
-            isUserValid=true;
-        } else{
-            changeBoxStyle(this.email, SignUp.errorBorder,true);
-            this.emailErrorMessage.setText("Email entered is invalid");
-            isUserValid=false;
-        }
-    }
-
-    private void changeBoxStyle(TextField field, String color, boolean visibility){
-        field.setStyle(field.getStyle() + color);
-        getAttribute(field.getId()).setVisible(visibility);
-
-    }
-
-    private boolean isEmptyBox(TextField field){
-        return field.getText().trim().isEmpty();
-    }
-
-    private  boolean isEmailValid(TextField email) {
-        Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(SignUp.EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email.getText());
-        return matcher.find();
-    }
+    @Override
     public void updatePasswordBoxes(){
         changeBoxStyle(this.password, SignUp.validBorder,false);
     }
 
-    private Label getAttribute(String attribute) {
+    @Override
+    public Label getAttribute(String attribute) {
         switch (attribute) {
             case "email": return this.emailErrorMessage;
             case "password": return this.passwordErrorMessage;
