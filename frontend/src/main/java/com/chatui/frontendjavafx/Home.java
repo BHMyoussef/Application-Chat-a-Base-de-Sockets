b5357c143f6bcc92a9725c95f6daa53a6a2dfb38
ZakariaDani <zakaria.dani2015@gmail.com>
2022-01-24 19:29:52 +0100

    update chat page

--- frontend/src/main/java/com/chatui/frontendjavafx/Home.java
+++ frontend/src/main/java/com/chatui/frontendjavafx/Home.java
@@ -2,20 +2,26 @@ package com.chatui.frontendjavafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.Buffer;
 import java.util.ArrayList;
 
@@ -42,11 +48,14 @@ public class Home extends Registration{
     @FXML
     private TextField messageBar;
     @FXML
-    private  Pane chatPage;
+    private ListView chatPage;
     @FXML
     private ImageView emo00, emo01, emo02, emo03, emo04, emo05, emo10, emo11, emo12, emo13, emo14, emo15, emo20, emo21, emo22, emo23, emo24, emo25, emo30, emo31, emo32, emo33, emo34, emo35;
     @FXML
     private Label nameProfile, emailProfile;
+    @FXML
+    private ScrollPane scrollPage;
+
     private Node last_msg_node;
 
     private boolean isEmojiMenuShown = false;
@@ -84,34 +93,27 @@ public class Home extends Registration{
                 addAllDescendents((Parent)node, nodes);
         }
     }
-    public void sendHandler(MouseEvent event) {
+    public void sendHandler(MouseEvent event) throws IOException {
+        send(event);
+    }
+
+    public void send(MouseEvent event) throws IOException {
         if(!messageBar.getText().trim().isEmpty()) {
             Label msg = new Label(messageBar.getText().trim());
             msg.setWrapText(true);
             msg.setTextAlignment(TextAlignment.JUSTIFY);
             msg.setMaxWidth(350.0);
-            msg.setMinWidth(messageBar.getLength());
-            last_msg_node = this.getAllNodes(chatPage).get(this.getAllNodes(chatPage).size() - 3);
             ImageView image = new ImageView(new Image("file:\\C:\\Users\\Zakaria Dani\\Desktop\\appChat\\Application-Chat-a-Base-de-Sockets\\frontend\\src\\main\\resources\\com\\chatui\\frontendjavafx\\icon\\me.png"));
             msg.setStyle("-fx-background-color: #50c984; -fx-background-radius: 20.0; -fx-font-family: Ebrima; -fx-font-size: 20.0; -fx-text-fill: #fff; -fx-padding: 5.0 5.0 5.0 5.0;");
-            chatPage.setMinHeight(chatPage.getHeight() + 500);
-            System.out.println(msg.getMinWidth());
-            if (msg.getMinWidth() <= 31) {
-                msg.setLayoutX(700 - 11.3 * msg.getMinWidth());
-                if (msg.getMinWidth() > 10)
-                    msg.setLayoutX(700 - 11.0 * msg.getMinWidth());
-            } else
-                msg.setLayoutX(700 - msg.getMaxWidth());
-            image.setLayoutY(last_msg_node.getLayoutY() + 108);
-            System.out.println(last_msg_node.getLayoutY());
-            msg.setLayoutY(last_msg_node.getLayoutY() + 135.0);
-            image.setLayoutX(708.0);
             image.setFitHeight(70.0);
+            msg.setTranslateY(25);
             image.setFitWidth(70.0);
-            chatPage.getChildren().add(msg);
-            chatPage.getChildren().add(image);
+            HBox x = new HBox();
+            x.setMaxWidth(chatPage.getWidth() - 20);
+            x.setAlignment(Pos.TOP_RIGHT);
+            x.getChildren().addAll(msg, image);
+            chatPage.getItems().add(x);
             messageBar.setText("");
-            System.out.println(last_msg_node);
         }
     }
     public void ShowEmojis(MouseEvent event) {
